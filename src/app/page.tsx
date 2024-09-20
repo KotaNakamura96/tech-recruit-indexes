0af59af6dc96aea90b9fa6b1bbf1083a3b47a802
kota1130 <kota.nkmr1130@gmail.com>
2024-09-20 23:46:14 +0900

    ダミーデータをもとに企業一覧を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import Image from 'next/image';
+import { promises as fs } from 'fs';
+import path from 'path';
+import CompanyList from '@/app/components/CompanyList';
+import { Company } from '@/app/types';
 
-export default function Home() {
-    return <div></div>;
-}
+const getCompanies = async (): Promise<Company[]> => {
+    const filePath = path.join(process.cwd(), 'src/app/data', 'companies.json');
+    const fileContents = await fs.readFile(filePath, 'utf8');
+    return JSON.parse(fileContents).companies;
+};
+
+const Home = async () => {
+    const companies = await getCompanies();
+
+    return <CompanyList companies={companies} />;
+};
+
+export default Home;
